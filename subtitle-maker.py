15d063b556745f11b1710d0afe6a4b08875c520a
uiyoung <suy2suy@gmail.com>
2022-12-24 12:49:41 +0900

    change to combine every 3 line

--- subtitle-maker.py
+++ subtitle-maker.py
@@ -6,17 +6,21 @@ argument = sys.argv
 
 
 # read lyrics text file
-f = open(argument[1] + '.txt', 'r', encoding='UTF-8')
+f = open('./target/' + argument[1] + '.txt', 'r', encoding='UTF-8')
 lines = f.readlines()
-lines = list(filter(lambda s: s != ('\u200b\n' and '\n'), lines))  # remove emptyline
+lines = list(map(lambda s: s.strip(), lines))  # 각줄의 양끝 개행문자 제거
+# lines = list(filter(lambda s: s not in ['', '\n', '\u200b\n' ',\u200b'], lines))  # remove empty line
 
-# combine 2 elements from lines
+# insert '' to every 3rd line
+for i in range(2, len(lines), 3):
+    if lines[i] != '':
+        lines.insert(i, '')
+
+
+# combine 3 elements from lines
 lyrics = []
-for i in range(0, len(lines), 2):
-    lyrics.append(''.join(lines[i:i+2]))
-
-# 개행문자 제거
-lyrics = list(map(lambda s: s.strip(), lyrics))
+for i in range(0, len(lines), 3):
+    lyrics.append('\n'.join(lines[i:i+3]).strip())
 
 
 # open template
@@ -24,6 +28,7 @@ presentation = Presentation('template.pptx')
 
 # set title
 song_title = argument[1]
+
 title_layout = presentation.slide_layouts[0]
 slide = presentation.slides.add_slide(title_layout)
 
@@ -32,12 +37,12 @@ title_placeholder.text = song_title
 
 subtitle_layout = presentation.slide_layouts[1]
 
-# make subtitle slides
-for i in range(len(lyrics)):
+# genrate subtitle slides with lyrics
+for subtitle in lyrics:
     slide = presentation.slides.add_slide(subtitle_layout)
     title_placeholder = slide.shapes.title
-    title_placeholder.text = lyrics[i]
+    title_placeholder.text = subtitle
 
 
 # save
-presentation.save(song_title + '.pptx')
+presentation.save('./result/' + song_title + '.pptx')
